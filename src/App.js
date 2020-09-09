1a3d99353b7957bc7b5eb1d72e75d835d30aeaa3
Alena Katkova <katkova.web@gmail.com>
2020-09-09 17:02:28 +0200

    generate and show matrix

--- src/App.js
+++ src/App.js
@@ -2,23 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function generateRandomInt() {
+  return Math.floor(Math.random() * 11) + 1;
+}
+
+function generateMatrix(m, n) {
+  let matrix = [];
+  let currentRow = [];
+  let currentInt = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      currentRow.push({
+        value: currentInt,
+        row: i,
+        column: j
+      });
+      currentInt++;
+    }
+    matrix.push(currentRow);
+    currentRow = [];
+  }
+  return matrix;
+}
+
+function drawMatrix(matrix) {
+  return matrix.map((row, index) => {
+    return <div key={`row${index}`}>{row.map(el => {
+      return <button key={`element${el.row}${el.column}`}>{el.value}</button>
+    })}</div>;
+  });
+}
+
 function App() {
+  let m = generateRandomInt();
+  let n = generateRandomInt();
+  let matrix = generateMatrix(m, n);
+  console.log(matrix);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {drawMatrix(matrix)}
     </div>
   );
 }
